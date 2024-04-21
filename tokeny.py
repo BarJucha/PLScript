ba158ee060aeba56262760184bf68c007f91a15d
BarJucha <bjucha@studnet.agh.edu.pl>
2024-04-21 18:22:02 +0200

    Dodanie tokenów 3

--- tokeny.py
+++ tokeny.py
@@ -18,7 +18,7 @@ tokens = (
     'PNAWIAS_KWADRATOWY',
     'LNAWIAS_KLAMROWY',
     'PNAWIAS_KLAMROWY'
-    'RÓWNE',
+    'ROWNE',
     'ROZNE',
     'PODSTAW',
     'WIEKSZE',
@@ -43,7 +43,7 @@ tokens = (
     'DWUKROPEK',
     'NAPIS',
     'PRZERWIJ',
-    'KONTUNUUJ',
+    'KONTYNUUJ',
     'ZWIEKSZ'
 )
 
@@ -54,5 +54,43 @@ def t_NUMBER(t):
     t.value = int(t.value)
     return t
 
-t_PLUS = r'DODAJ'
-t_MINUS = r'ODEJMIJ'
+t_PLUS = r'POLACZ'
+t_MINUS = r'ROZLACZ'
+t_MNOZENIE = r'MULTI'
+t_DZIELENIE = r'ROZWAL NA'
+t_POTEGOWANIE = r'POTEGA'
+t_KOMENTARZ = r'SWOJA DROGA'
+t_FUNKCJA = r'MAGIA'
+t_LNAWIAS_OKRAGLY = r'\('
+t_PNAWIAS_OKRAGLY = r'\)'
+t_LNAWIAS_KWADRATOWY = r'\['
+t_PNAWIAS_KWADRATOWY = r'\]'
+t_LNAWIAS_KLAMROWY = r'{'
+t_PNAWIAS_KLAMROWY = r'}'
+t_ROWNE = r'TO SAMO CO'
+t_ROZNE = r'INNE OD'
+t_PODSTAW = r'TO'
+t_WIEKSZE = r'SILNIEJSZE OD'
+t_MNIEJSZE = r'SLABSZE OD'
+t_MNIEJSZE_RÓWNE = r'<='
+t_WIEKSZE_RÓWNE = r'>='
+t_ALTERANTYWA = r'LUB'
+t_KONIUNKCJA = r'I'
+t_NEGACJA = r'NIE'
+t_PRZECINEK = r','
+t_WHILE = r'DO MOMENTU'
+t_FOR = r'PETLA PO'
+t_IF = r'JESLI'
+t_ELIF = r'LUB_JESLI'
+t_ELSE = r'INACZEJ'
+t_RETURN = r'ODDAJ'
+t_PRAWDA = r'PRAWDA'
+t_FALSZ = r'FALSZ'
+t_KLASA = r'KATEGORIA'
+t_FROM = r'OD'
+t_IMPORT = r'UKRADNIJ'
+t_DWUKROBEK = r':'
+t_NAPIS = t_NAPIS = r'"[^"\n]*"'
+t_PRZERWIJ = r'SKONCZ'
+t_KONTYNUUJ = r'LEC DALEJ'
+t_ZWIEKSZ = r'LVL_UP'
