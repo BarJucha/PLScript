7b7f4648c9e98876988877bf7dfd48c5b4b9b38d
BarJucha <bjucha@studnet.agh.edu.pl>
2024-04-21 14:27:34 +0200

    Dodanie tokenów 2

--- tokeny.py
+++ tokeny.py
@@ -38,6 +38,21 @@ tokens = (
     'PRAWDA',
     'FALSZ',
     'KLASA',
-
+    'FROM',
+    'IMPORT',
+    'DWUKROPEK',
+    'NAPIS',
+    'PRZERWIJ',
+    'KONTUNUUJ',
+    'ZWIEKSZ'
 )
 
+t_ZMIENNA = r'[a-zA-Z_][a-zA-Z0-9_]*'
+
+def t_NUMBER(t):
+    r'-?\d+\.?\d*'
+    t.value = int(t.value)
+    return t
+
+t_PLUS = r'DODAJ'
+t_MINUS = r'ODEJMIJ'
