b396ac46c0fe71ed02408e2727788778ec2ddb42
BarJucha <115100213+BarJucha@users.noreply.github.com>
2024-05-27 13:23:41 +0200

    Update Driver.py

--- Driver.py
+++ Driver.py
@@ -1,19 +1,23 @@
 from antlr4 import *
-from ANTLRGenerated.PLSRulesLexer import PLSRulesLexer
-from ANTLRGenerated.PLSRulesParser import PLSRulesParser
+from NewRules.antlr.PLSRulesV2Parser import PLSRulesV2Parser
+from NewRules.antlr.PLSRulesV2Lexer import PLSRulesV2Lexer
+from NewRules.antlr.PLSRulesV2Visitor import PLSRulesV2Visitor
+
 
 def parse_code(input_code):
     input_stream = InputStream(input_code)
-    lexer = PLSRulesLexer(input_stream)
+    lexer = PLSRulesV2Lexer(input_stream)
     token_stream = CommonTokenStream(lexer)
-    parser = PLSRulesParser(token_stream)
+    parser = PLSRulesV2Parser(token_stream)
     tree = parser.program()
     return tree
 
 def main():
     with open("plsCode.txt", 'r') as file:
         text = file.read()
-        parse_code(text)
+        tree = parse_code(text)
+        visitor = PLSRulesV2Visitor()
+        visitor.visit(tree)
 
 if __name__ == "__main__":
     main()
